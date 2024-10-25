9aaa06f5ed7eb240573786d8b6f720a19e7a826e
kimdayeon37 <kdy37912@naver.com>
2024-10-25 10:09:20 +0900

    test: 외부 모듈 및 라이브러리 mocking

--- test/payment.test.js
+++ test/payment.test.js
@@ -2,3 +2,13 @@ import { CheckoutPage } from '../src/pages/PaymentsPage/CheckoutPage.jsx';
 import { render, fireEvent, waitFor } from '@testing-library/react';
 import { loadTossPayments } from '@tosspayments/tosspayments-sdk';
 import { v4 as uuidv4 } from 'uuid';
+
+// loadTossPayments 함수를 모킹하여 결제 위젯을 반환
+jest.mock('@tosspayments/tosspayments-sdk', () => ({
+  loadTossPayments: jest.fn(),
+}));
+
+// uuidv4()를 모킹하여 테스트용 고정 customerKey('test-customer-key')를 반환
+jest.mock('uuid', () => ({
+  v4: jest.fn(),
+}));
